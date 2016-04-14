be5356be4a9003600abeb382e0b369f1328ee871
tomoaki <england0112@gmail.com>
2016-04-14 22:30:33 +0900

    install redux settings

---
+++ spec/components/app_test.js
@@ -0,0 +1,3 @@
+import { renderComponent , expect } from '../test_helper';
+import App from '../../src/components/app';
+
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/app';
+
+
+export default (
+    <Route path="/" component={App}>
+    </ Route>
+);
