2b46bb5781148db40b5d75abec9204e0f971e9c2
tomoaki <england0112@gmail.com>
2016-05-26 21:52:07 +0900

    create client folders

---
+++ src/client/routes.js
@@ -0,0 +1,25 @@
+// import React from 'react';
+// import { Route, IndexRoute } from 'react-router';
+//
+// import App from './components/App/index';
+// import PostsIndex from './containers/posts/Index/index';
+// import PostsForm from './containers/posts/Form/index';
+// import ProjectsIndex from './containers/projects/Index/index';
+// import ProjectsForm from './containers/projects/Form/index';
+// import HomesIndex from './containers/homes/Index/index';
+// import AuthorsIndex from './containers/authors/Index/index';
+// import AuthorsForm from './containers/authors/Form/index';
+// import AuthorsSinUp from './containers/authors/SignUp/index';
+// import AuthorsSingIn from './containers/authors/SignIn/index';
+// import NotFound from './components/NotFound/index';
+//
+// export default (
+//   <Route path="/" component={App}>
+//     <IndexRoute component={Home} />
+//       <Route path="/posts" component={PostsIndex} />
+//       <Route path="/posts/:id" component={Post} />
+//       <Route path="/about" component={Author} />
+//       <Route path="/projects" component={ProjectsIndex} />
+//       <Route path="*" component={NotFound} />
+//   </ Route>
+// );
