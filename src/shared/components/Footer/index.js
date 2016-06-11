c150515cc7967bf9f77e38b97d31254ab99b6952
tomoaki <england0112@gmail.com>
2016-06-11 14:07:53 +0900

    create footer

---
+++ src/shared/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react'
+import styles from './styles.scss';
+
+
+export function Footer() {
+  return (
+    <section className={styles.root}>
+      <cite>created by <a href="https://github.com/tsurupin"> TOMOAKI TSURUTA</a></cite>
+    </section>
+  )
+};
