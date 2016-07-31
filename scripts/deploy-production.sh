a52cdd019e7dfb38fd7d5cbb7a6c330942543061
tomoaki <england0112@gmail.com>
2016-07-31 18:43:37 +0900

    add enviroment variables to deploy scripts

--- scripts/deploy-production.sh
+++ scripts/deploy-production.sh
@@ -2,9 +2,11 @@
 
 set -ex
 
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
 export AWS_DEFAULT_REGION="ap-us-west-1"
-
 MY_SECURITY_GROUP=${AWS_SECURITY_GROUP_ID}
+
 MY_IP=`curl -s ifconfig.me`
 
 aws ec2 authorize-security-group-ingress --group-id $MY_SECURITY_GROUP --protocol tcp --port 22 --cidr $MY_IP/32 --debug
